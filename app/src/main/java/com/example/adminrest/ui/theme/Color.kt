d16f3c3d42e092cfd04a70a10a10fbba57572d49
JuanJpsh <113944776+JuanJpsh@users.noreply.github.com>
2023-06-09 01:55:23 -0500

    login page done

---
+++ app/src/main/java/com/example/adminrest/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.example.adminrest.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Orange = Color(0xFFF57336)
+val CreamOrange = Color(0xFFFDEEE6)
+val IntermediateOrange = Color(0xFFFBC7AF)
